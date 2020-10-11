ddda1a20590dab131496f88af4b9637c07d62666
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-10-11 13:33:53 +0200

    add cors middleware for local testing

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import routes from "./lib/routes";
 import bodyParser from "body-parser";
+import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(routes);
 
