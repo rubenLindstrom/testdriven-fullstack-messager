3bc62cb1305dd0a7b71ae53cb1087d07df515a4b
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-10-17 12:06:48 +0200

    reverse order of new posts

--- backend/lib/controller.js
+++ backend/lib/controller.js
@@ -3,45 +3,44 @@ import MessageApp from "./model";
 let messageApp;
 
 const DB_PATH = {
-	TEST: "////json///testMessages.json",
-	PROD: "////json///messages.json"
+  TEST: "////json///testMessages.json",
+  PROD: "////json///messages.json",
 };
 const IS_TEST = process.env.npm_lifecycle_event === "test";
 
 messageApp = new MessageApp(IS_TEST ? DB_PATH.TEST : DB_PATH.PROD);
 
 export const getSingleMessage = (id) =>
-	new Promise((resolve, reject) => {
-		const message = messageApp.get(id);
-		if (message) resolve(message);
-		else reject("Message not found in database");
-	});
+  new Promise((resolve, reject) => {
+    const message = messageApp.get(id);
+    if (message) resolve(message);
+    else reject("Message not found in database");
+  });
 
 export const getAll = () =>
-	new Promise((resolve, reject) => {
-		const messages = messageApp.getAll();
-		console.log(messages.length);
-		if (messages.length !== 0) resolve(messages);
-		else reject("No messages in database");
-	});
+  new Promise((resolve, reject) => {
+    const messages = messageApp.getAll();
+    if (messages.length !== 0) resolve(messages);
+    else reject("No messages in database");
+  });
 
 export const post = (content) =>
-	new Promise((resolve, reject) => {
-		const message = messageApp.post(content);
-		if (message.length !== 0) resolve(message);
-		else reject("You can't post an empty message");
-	});
+  new Promise((resolve, reject) => {
+    const message = messageApp.post(content);
+    if (message.length !== 0) resolve(message);
+    else reject("You can't post an empty message");
+  });
 
 export const deleteMessage = (id) =>
-	new Promise((resolve, reject) => {
-		const result = messageApp.delete(id);
-		if (result !== "Message not found in database") resolve(result);
-		else reject(result);
-	});
+  new Promise((resolve, reject) => {
+    const result = messageApp.delete(id);
+    if (result !== "Message not found in database") resolve(result);
+    else reject(result);
+  });
 
 export const updateMessage = (id, content) =>
-	new Promise((resolve, reject) => {
-		const result = messageApp.update(id, content);
-		if (result.length !== 0) resolve(result);
-		else reject("Message not found in database");
-	});
+  new Promise((resolve, reject) => {
+    const result = messageApp.update(id, content);
+    if (result.length !== 0) resolve(result);
+    else reject("Message not found in database");
+  });
--- backend/lib/model.js
+++ backend/lib/model.js
@@ -1,73 +1,73 @@
 import fs from "fs";
 import path from "path";
 
-const getNewId = (arr) => (arr.length ? arr.slice(-1)[0].id + 1 : 1);
+const getNewId = (arr) => (arr.length ? arr[0].id + 1 : 1);
 
 class MessageApp {
-	constructor(filePath) {
-		this.filePath = filePath;
-		this.messages = filePath ? this.readFromJson() : [];
-	}
+  constructor(filePath) {
+    this.filePath = filePath;
+    this.messages = filePath ? this.readFromJson() : [];
+  }
 
-	post(content) {
-		if (!content) return [];
-		this.messages.push({
-			id: getNewId(this.messages),
-			content,
-			date: new Date()
-		});
-		this.writeToJSON();
-		return this.messages;
-	}
+  post(content) {
+    if (!content) return [];
+    this.messages.unshift({
+      id: getNewId(this.messages),
+      content,
+      date: new Date(),
+    });
+    this.writeToJSON();
+    return this.messages;
+  }
 
-	get(id) {
-		return this.messages.find((el) => el.id == id);
-	}
+  get(id) {
+    return this.messages.find((el) => el.id == id);
+  }
 
-	getAll() {
-		return this.messages;
-	}
+  getAll() {
+    return this.messages;
+  }
 
-	update(id, content) {
-		const idx = this.messages.findIndex((el) => el.id == id);
-		if (idx == -1) return [];
+  update(id, content) {
+    const idx = this.messages.findIndex((el) => el.id == id);
+    if (idx == -1) return [];
 
-		this.messages[idx].content = content;
-		this.writeToJSON();
-		return this.messages[idx];
-	}
+    this.messages[idx].content = content;
+    this.writeToJSON();
+    return this.messages[idx];
+  }
 
-	delete(id) {
-		const idx = this.messages.findIndex((el) => el.id == id);
-		if (idx == -1) return "Message not found in database";
+  delete(id) {
+    const idx = this.messages.findIndex((el) => el.id == id);
+    if (idx == -1) return "Message not found in database";
 
-		this.messages.splice(idx, 1);
-		this.writeToJSON();
-		return this.messages;
-	}
+    this.messages.splice(idx, 1);
+    this.writeToJSON();
+    return this.messages;
+  }
 
-	readFromJson() {
-		return JSON.parse(
-			fs.readFileSync(
-				__dirname + path.normalize(this.filePath),
-				"utf8",
-				(err, data) => {
-					if (err) throw err;
-				}
-			)
-		);
-	}
+  readFromJson() {
+    return JSON.parse(
+      fs.readFileSync(
+        __dirname + path.normalize(this.filePath),
+        "utf8",
+        (err, data) => {
+          if (err) throw err;
+        }
+      )
+    );
+  }
 
-	writeToJSON() {
-		if (this.filePath)
-			fs.writeFileSync(
-				__dirname + path.normalize(this.filePath),
-				JSON.stringify(this.messages),
-				(err) => {
-					if (err) throw err;
-				}
-			);
-	}
+  writeToJSON() {
+    if (this.filePath)
+      fs.writeFileSync(
+        __dirname + path.normalize(this.filePath),
+        JSON.stringify(this.messages),
+        (err) => {
+          if (err) throw err;
+        }
+      );
+  }
 }
 
 export default MessageApp;
